93b81c4f238b0fc6a80bc1dde7932d125ff4e6bc
Mohamed Soliman <forcodin23@gmail.com>
2025-06-09 14:54:51 +0300

    Up to Date With the original project

---
+++ index.js
@@ -0,0 +1,41 @@
+//npm i express body-parser ejs mysql2 slashes@2.0.0 md5 cookie-parser
+const port = 7777;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+const path = require("path");
+app.use(bodyParser.urlencoded({extended: false}));
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+global.jwt = require('jsonwebtoken');
+
+let db_M = require('./database');
+global.db_pool = db_M.pool;
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "./views"));
+
+global.addSlashes    = require('slashes').addSlashes;
+global.stripSlashes  = require('slashes').stripSlashes;
+
+global.was_logged = false;
+const user_Mid = require("./middleware/user_Mid");
+
+const crs_R = require('./Routers/course_R');
+app.use('/Crs',[user_Mid.isLogged],crs_R);
+const usr_R = require('./Routers/users_R');
+app.use('/U',[user_Mid.isLogged],usr_R);
+const auth_R = require('./Routers/auth_R');
+app.use('/',auth_R);
+
+
+app.get('/', (req, res) => {
+    res.render("index", {});
+})
+
+app.listen(port, () => {
+    console.log(`Now listening on port http://localhost:${port}`);
+});
