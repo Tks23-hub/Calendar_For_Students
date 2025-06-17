5ef022b54ba1c421c742035d19319161cbfc806a
Mohamed Soliman <forcodin23@gmail.com>
2025-06-17 11:57:52 +0300

    תכנון פעילות (עם באג)

---
+++ Routers/Activity_R.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+
+const Activity_Mid = require("../middleware/Activity_Mid");
+const course_Mid = require("../middleware/course_Mid");
+
+router.get("/plan", [course_Mid.GetAllCourses], (req, res) => {
+  res.render("activity_add", {
+    courses: req.courses_data,
+    data: {},
+  });
+});
+router.post("/plan", [Activity_Mid.AddNewActivity], (req, res) => {});
--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 //npm i express body-parser ejs mysql2 slashes@2.0.0 md5 cookie-parser
 const port = 7777;
-const express = require('express');
+const express = require("express");
 const app = express();
 app.use(express.json());
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const path = require("path");
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 app.use(cookieParser());
-global.jwt = require('jsonwebtoken');
+global.jwt = require("jsonwebtoken");
 
-let db_M = require('./database');
+let db_M = require("./database");
 global.db_pool = db_M.pool;
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, "./views"));
+app.set("views", path.join(__dirname, "./views"));
 
-global.addSlashes    = require('slashes').addSlashes;
-global.stripSlashes  = require('slashes').stripSlashes;
+global.addSlashes = require("slashes").addSlashes;
+global.stripSlashes = require("slashes").stripSlashes;
 
 global.was_logged = false;
 const user_Mid = require("./middleware/user_Mid");
+const actv_R = require("./Routers/Activity_R");
+app.use("/A", [user_Mid.isLogged], actv_R);
+const crs_R = require("./Routers/course_R");
+app.use("/Crs", [user_Mid.isLogged], crs_R);
+const usr_R = require("./Routers/users_R");
+app.use("/U", [user_Mid.isLogged], usr_R);
+const auth_R = require("./Routers/auth_R");
+app.use("/", auth_R);
 
-const crs_R = require('./Routers/course_R');
-app.use('/Crs',[user_Mid.isLogged],crs_R);
-const usr_R = require('./Routers/users_R');
-app.use('/U',[user_Mid.isLogged],usr_R);
-const auth_R = require('./Routers/auth_R');
-app.use('/',auth_R);
-
-
-app.get('/', (req, res) => {
-    res.render("index", {});
-})
+app.get("/", (req, res) => {
+  res.render("index", {});
+});
 
 app.listen(port, () => {
-    console.log(`Now listening on port http://localhost:${port}`);
+  console.log(`Now listening on port http://localhost:${port}`);
 });
